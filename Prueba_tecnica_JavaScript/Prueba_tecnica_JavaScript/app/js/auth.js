be59f4bb386f2445548ed1a66806143171a8ab0f
ReijBak <stevencardona2001@gmail.com>
2026-01-21 17:27:32 -0500

    Fix: Double directory

--- Prueba_tecnica_JavaScript/Prueba_tecnica_JavaScript/app/js/auth.js
+++
@@ -1,6 +0,0 @@
-export function guardian() {
-    let auth = sessionStorage.getItem('Auth');
-    if (!auth || auth !== 'true') {
-        window.location = '../views/login.html'
-    }
-}
--- Prueba_tecnica_JavaScript/Prueba_tecnica_JavaScript/app/views/not-found.js
+++
@@ -1,4 +0,0 @@
-export function notFound() {
-    const cont = `<h1>404</h1><p>Página no encontrada.</p>`
-    return cont
-}
